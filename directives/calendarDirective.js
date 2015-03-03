f9d5d8ea6010c32c9c66e757bf661d7c5a7de390
Lee Leathers <leeleathers@gmail.com>
2015-03-02 17:43:05 -0800

    chore(calendarDirective.js): s/COMPONENTS_BASE_URL/VENDOR_BASE_URL

--- directives/calendarDirective.js
+++ directives/calendarDirective.js
@@ -1,12 +1,12 @@
 angular.module('cn.calendar')
-    .directive('calendarCard', ['COMPONENTS_BASE_URL', function(COMPONENTS_BASE_URL) {
+    .directive('calendarCard', ['VENDOR_BASE_URL', function(VENDOR_BASE_URL) {
       return {
         restrict: 'E',
         transclude: true,
         scope: {
           calendar: '='
         },
-        templateUrl: COMPONENTS_BASE_URL + 'cn-calendar/partials/calendar.html',
+        templateUrl: VENDOR_BASE_URL + 'angular-calendar/partials/calendar.html',
         controller: 'CalendarController'
       };
     }]);
